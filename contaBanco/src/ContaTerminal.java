46924865cbd54ad2576f3912db69de01712816dc
Douglas Mariano <doug_pinda@hotmail.com>
2023-08-31 21:21:52 -0300

    criando conta e inserindo dados via Scanner

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
-public class ContaTerminal {
+import java.util.Scanner;
+
+public class ContaTerminal{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double saldo = 200;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Por favor, entre com seu Nome: ");
+        String nome = entrada.next();
+
+        System.out.println("Agora, entre com a agência: ");
+        String agencia = entrada.next();
+
+        System.out.println("Agora, entre com o número: ");
+        Integer numero = entrada.nextInt();
+
+        System.out.println("Olá, " + nome + "! Obrigado por criar uma conta em nosso banco, sua agencia é: "
+                + agencia + " conta: " + numero + " e seu saldo de $" + saldo + " já está disponível para saque.");
+
     }
 }
